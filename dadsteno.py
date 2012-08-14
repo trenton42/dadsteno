3eeabb66ed6a0b90917a1151fad629b6fff477a0
Trenton Broughton <trenton@devpie.com>
2012-08-13 23:20:17 -0400

    Code Clean Up
    Moved some things around, added some comments, and made the error
    messages even less helpful!

--- dadsteno.py
+++ dadsteno.py
@@ -1,7 +1,11 @@
+import argparse
+import sys
+import os
+import struct
+import random
 
 from PIL import Image
 from collections import deque
-import argparse, sys, os, struct, random
 
 def main():
 	parser = argparse.ArgumentParser()
@@ -20,7 +24,7 @@ def main():
 			decrypt(args.image, args.file, password=password)
 		else:
 			encrypt(args.file, args.image, password=password)
-	except IOError as err:
+	except Exception as err:
 		print 'A bad bad thing happened: ', err
 		sys.exit(1)
 
@@ -28,13 +32,13 @@ def encrypt(inf, outf, password=None):
 	try:
 		fp = open(inf, 'r')
 	except IOError:
-		raise Exception('Yer input file is NO GOOD. Take it back. Here: '+str(inf))
+		raise Exception('Yer input file is NO GOOD. Take it back!')
 	
 	fs = os.stat(inf).st_size
 	try:
 		im = Image.open(outf)
 	except IOError:
-		raise Exception('Yer output file is Poopy. Take it back. Here: '+str(inf))
+		raise Exception('Yer output file Does NOT work for me. Take it back!')
 	av = getAvail(im)
 	if fs+8 > av:
 		raise Exception("It's not a TARDIS, ya know. Ye got too much stuff!")
@@ -47,13 +51,13 @@ def decrypt(inf, outf, password=None):
 	try:
 		fp = open(outf, 'w')
 	except IOError:
-		raise Exception('Yer input file is NO GOOD. Take it back. Here: '+str(inf))
+		raise Exception('How can you expect me to work in these conditions?! The file you gave me to use for YOUR data does not work!')
 	
 	try:
 		im = Image.open(inf)
 	except IOError:
-		raise Exception('Yer output file is Poopy. Take it back. Here: '+str(inf))
-	im = im.convert('RGBA')
+		raise Exception("I don't know what you did with this image, but it doesn't work at all.")
+	im = im.convert('RGB')
 	g = unmergeData(im, password)
 	size = ''
 	for i in range(8):
@@ -62,9 +66,9 @@ def decrypt(inf, outf, password=None):
 	try:
 		size = struct.unpack('Q',size)[0]
 	except Exception as err:
-		raise Exception('Looks like your image is no no good.')
+		raise Exception("You probably did something wrong. There is nothing like trying to find something where nothing exists.")
 	if size+8 > getAvail(im):
-		raise Exception('Looks like your image is no no good. sz')
+		raise Exception("You probably did something wrong. There is nothing like trying to find something where nothing exists.")
 	for i in range((size)):
 		fp.write(g.next())
 	
@@ -78,6 +82,7 @@ def getAvail(im):
 	return tot
 
 def filePrep(fp, pg):
+	''' Generator that yields the stream of data one bit at a time '''
 	size = struct.pack('Q', os.fstat(fp.fileno()).st_size)
 	for i in size:
 		num = ord(i)
@@ -100,6 +105,9 @@ def filePrep(fp, pg):
 			yield bit
 
 def mergeData(im, fp, password=None):
+	''' Generator that yields each pixel of the image,
+	merging input data with the least significant bit of each color channel.
+	'''
 	g = iter(im.getdata())
 	if password is not None:
 		pg = passGen(password)
@@ -121,7 +129,7 @@ def mergeData(im, fp, password=None):
 		yield i
 
 def unmergeData(im, password=None):
-	''' Generator that splits the data out of the image stream returning one character at a time '''
+	''' Generator yields each character of the hidden data after a full byte has been collected '''
 	if password is not None:
 		pg = passGen(password)
 	else:
@@ -142,7 +150,7 @@ def unmergeData(im, password=None):
 		yield chr(out)
 
 def passGen(password):
-	''' Generator that returns eight bits of psudo random data at a time seeded with the supplied password '''
+	''' Generator that yields eight bits of psudo random data at a time seeded with the supplied password '''
 	random.seed(password)
 	state = random.getstate()
 	while True:
